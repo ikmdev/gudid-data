3f7cdd644a3f8f369672efef7ff238409c0dc5f1
Greg Liebowitz <gliebowitz@deloitte.com>
2025-06-17 16:02:11 -0400

    IKDT-1034 Handle both FOI and GUDID origin ZIPs (#6)

--- plugin/gudid-transformation-maven-plugin/src/main/java/dev/ikm/maven/AbstractTransformer.java
+++ plugin/gudid-transformation-maven-plugin/src/main/java/dev/ikm/maven/AbstractTransformer.java
@@ -1,13 +1,19 @@
 package dev.ikm.maven;
+
 import java.util.UUID;
 
 public abstract class AbstractTransformer implements Transformer {
-    final UUID namespace;
+    protected final UUID namespace;
+    protected final GudidUtility gudidUtility;
+
     AbstractTransformer(UUID namespace) {
         this.namespace = namespace;
+        this.gudidUtility = new GudidUtility(namespace);
     }
+
     @Override
     public UUID getNamespace() {
         return namespace;
     }
+
 }
